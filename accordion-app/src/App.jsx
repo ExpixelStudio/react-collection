3b83b258e63a9ed31f0e8e6dca5cfbef271f3877
gtlegend <yusuf_ar@live.com>
2024-10-04 06:35:24 -0400

    corrected project name and components from accordian to accordion*.

---
+++ accordion-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Accordian from './components/accordion'
+
+function App() {
+  return (
+    <>
+      {/* Accordian Component */}
+      <Accordian/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ accordion-app/src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: 1,
+        question: 'What are accordion components?',
+        answer : 'Yes indeed',
+    },
+    {  
+        id: 2,
+        question: 'What are they used for?',
+        answer : 'No indeed not',
+    },
+    {
+        id:3,
+        question: 'Accordion as a musical instrument',
+        answer : 'Quite possibly my boy',
+    },
+    {
+        id:4,
+        question: 'Can i create accordion components with a different framework?',
+        answer: 'This is surely impossible',
+    },
+];
+
+export default data;
