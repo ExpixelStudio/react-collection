d6e1cd46b8d6aed28b4d13f951e2021ee0006ca1
gtlegend <yusuf_ar@live.com>
2024-10-05 17:09:48 -0400

    updated info in data.js. Cpmpleted functionality for single item selection to display data. Added css styling to accordion component.

--- accordion-app/src/components/accordion/data.js
+++ accordion-app/src/components/accordion/data.js
@@ -2,22 +2,22 @@ const data = [
     {
         id: 1,
         question: 'What are accordion components?',
-        answer : 'Yes indeed',
+        answer : 'Accordion components are user interface elements used for organizing and presenting content in a collapsible manner. They typically consist of a header, content and an expand/collapse action.',
     },
     {  
         id: 2,
         question: 'What are they used for?',
-        answer : 'No indeed not',
+        answer : 'They are commonly employed in various contexts, including FAQs, product drescriptions, navigation menus, settings panels and data tables, to save screen space and provide a structured and user-friendly interface for presenting information or options.',
     },
     {
         id:3,
         question: 'Accordion as a musical instrument',
-        answer : 'Quite possibly my boy',
+        answer : 'The accordion is a musical instrument with a keyboard and bellows. It produces sound by air passing over reeds when the player expands and compresses the bellows, used in various music genres.',
     },
     {
         id:4,
         question: 'Can i create accordion components with a different framework?',
-        answer: 'This is surely impossible',
+        answer: 'Yes of course, it is very possible to create an accordion component with another framework.',
     },
 ];
 
--- accordion-app/src/components/accordion/index.jsx
+++ accordion-app/src/components/accordion/index.jsx
@@ -1,24 +1,45 @@
 import { useState } from "react";
 
 import data from "./data";
-
+import './style.css';
 
 export default function Accordion(){
 
-    const  [selected, setSelected] = useState(null);
+    const  [selected, setSelected] = useState(null); //stores selected item ID for single selection.
+    const [enableMultiSelect, setEnableMultiSelect] = useState(false); //enable or disable multi selection.
+    const [multiple, setMultiple] = useState([]); // stores multiple IDs for multi selection.
 
+    function handleSingleSelection(getCurrentId){
+        //console.log(getCurrentId);
+        setSelected(getCurrentId === selected ? null : getCurrentId); //closes component if already open
+    }
+
+    console.log(selected);
     return (
         <div className="wrapper">
+            <button>Enable Multi-Selection</button>
             <div className="accordion">
                 {
-                    data && data.length > 0 ? 
-                    data.map(dataItem => <div className="item">
-                        <div className="title">
-                            <h3>{dataItem.question}</h3>
-                            <span>+</span>
-                        </div>
-                    </div> )
-                    : <div>No data Found!</div>
+                    data && data.length > 0 ? (
+                    data.map((dataItem) => (
+                        <div className="item">
+                            <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
+                                <h3>{dataItem.question}</h3>
+                                <span>+</span>
+                            </div>
+                            {
+                                selected === dataItem.id ? 
+                                <div className="content">{dataItem.answer}</div>
+                                : null
+                                
+                            }
+                            
+                        </div> 
+                        ))
+                    )
+                    :( 
+                        <div>No data Found!</div>
+                    )
                 }
             </div>
         </div>
