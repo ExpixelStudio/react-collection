d44fa7dea3fe85b49663d076cee271ec642a038b
gtlegend <yusuf_ar@live.com>
2024-10-14 16:11:12 -0400

    added logic for generating random RGB color and some html/css to display color type to screen.

--- random-color-app/src/components/index.jsx
+++ random-color-app/src/components/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function RandomColor(){
@@ -23,8 +23,17 @@ export default function RandomColor(){
     }
 
     function handleCreateRandomRgbColor() {
+        const r= randomColorUtility(256);
+        const g= randomColorUtility(256);
+        const b= randomColorUtility(256);
 
+        setColor(`rgb(${r},${g},${b})`);
     }
+
+    useEffect(()=>{
+        if (typeOfColor === 'rgb') handleCreateRandomRgbColor();
+            else handleCreateRandomHexColor();
+    },[typeOfColor]);
 
     return (
         <div 
@@ -38,6 +47,20 @@ export default function RandomColor(){
             <button onClick={() => setTypeOfColor('hex')}>Create Hex Color</button>
             <button onClick={() => setTypeOfColor('rgb')}>Create RGB Color</button>
             <button onClick={typeOfColor === 'hex' ? handleCreateRandomHexColor : handleCreateRandomRgbColor}>Generate Random Color</button> 
+
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                color: '#fff',
+                fontSize: '50px',
+                marginTop: '50px',
+                flexDirection: 'column',
+                gap: '20',
+            }}>
+                <h2>{typeOfColor === 'rgb' ? 'RGB Color' : 'HEX Color'}</h2>
+                <h1>{color}</h1>
+            </div>
         </div>
     );
 }
