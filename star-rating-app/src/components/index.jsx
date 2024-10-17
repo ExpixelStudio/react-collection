7cab87507215d06cb93a47c1a2a27011cf0b8e7e
gtlegend <yusuf_ar@live.com>
2024-10-17 05:31:21 -0400

    Completed.

--- star-rating-app/src/components/index.jsx
+++ star-rating-app/src/components/index.jsx
@@ -1,35 +1,42 @@
 import { useState } from 'react';
 import {FaStar} from 'react-icons/fa';
+import './style.css';
 
-/* const [rating, setRating] = useState(0);
-const [hover, setHover] = useState(0); */
 
-function handleClick(getCurrentIndex) {
-    console.log(getCurrentIndex);
-}
+export default function StarRating({noOfStars = 6}){ //if no value is passed, default value will be 5
 
-function handleMouseEnter(getCurrentIndex) {
-    console.log(getCurrentIndex);
-}
+        const [rating, setRating] = useState(0);
+        const [hover, setHover] = useState(0);
 
-function handleMouseLeave(getCurrentIndex) {
-    console.log(getCurrentIndex);
-}
+        function handleClick(getCurrentIndex) {
+        console.log(getCurrentIndex);
+        setRating(getCurrentIndex);
+        }
 
-export default function StarRating({noOfStars = 5}){ //if no value is passed, default value will be 5
+        function handleMouseEnter(getCurrentIndex) {
+        console.log(getCurrentIndex);
+        setHover(getCurrentIndex);
+        }
+
+        function handleMouseLeave() {
+        /* console.log(getCurrentIndex); */
+        setHover(rating);
+        }
+
     return (
         <div className="star-rating">
             {
-                [...Array(noOfStars)].map((_,index) => {
+                [...Array(noOfStars)].map((_, index) => {  /*  _ is a placeholder for the first argument in the callback (value) which is being ignored/not used. So that the 2nd argument (index) can be utilized */   
 
-                    index += 1;
-
+                    index += 1; //starts index count from 1-5 instead of 0-4
+                    
                     return (
                         <FaStar 
                             key ={index}
+                            className= {index <= (hover || rating) ? 'active star' : 'inactive star'}
                             onClick={() => handleClick(index)}
-                            onMouseMove={() => handleMouseEnter(index)}
-                            onMouseLeave={() => handleMouseLeave(index)}
+                            onMouseEnter={() => handleMouseEnter(index)}
+                            onMouseLeave={() => handleMouseLeave()}
                             size={40}
                         />
                     );
