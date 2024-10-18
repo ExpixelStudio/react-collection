3c653f2e8cb88c60d1ddd803d0d30760aa4547aa
gtlegend <yusuf_ar@live.com>
2024-10-18 07:59:42 -0400

    added logic handing next and previous img on arrow click and selecting img by circle selector.

--- image-slider-app/src/components/image-slider/index.jsx
+++ image-slider-app/src/components/image-slider/index.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import {BsArrowLeftCircleFil, BsArrowRightCircleFill} from 'react-icons/bs'
-import '../../App.css';
+import {BsArrowLeftCircleFill, BsArrowRightCircleFill} from 'react-icons/bs'
+/* import '../../App.css'; */
+import './style.css';
 
 export default function ImageSlider({url, limit = 5, page = 1}) {
 
@@ -34,6 +35,14 @@ export default function ImageSlider({url, limit = 5, page = 1}) {
 
     console.log(images);
 
+    function handlePrevious(){
+        setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1)
+    }
+
+    function handleNext(){
+        setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1)
+    }
+
     if(loading) {
         return <div>Loading data! Please wait.</div>
     }
@@ -44,7 +53,37 @@ export default function ImageSlider({url, limit = 5, page = 1}) {
 
     return (
         <div className="container">
-            <h1>DOJ</h1>
+            <BsArrowLeftCircleFill onClick={handlePrevious} className='arrow arrow-left' />
+            {
+                images && images.length ? 
+                    images.map((imageItem, index) => (
+                        <img 
+                            key={imageItem.id}
+                            alt={imageItem.download_url}
+                            src={imageItem.download_url}
+                            className={currentSlide === index 
+                                ? 'current-image' 
+                                : 'current-image hide-current-image'}
+                        />
+                    ))
+                : null
+            }
+
+            <BsArrowRightCircleFill onClick={handleNext} className="arrow arrow-right" />
+            <span className="circle-indicators">
+                {
+                    images && images.length ? 
+                        images.map((_,index)=> <button
+                        key={index}
+                        className={
+                            currentSlide === index 
+                                ? 'current-indicator' 
+                                : 'current-indicator hide-current-indicator'}
+                        onClick={() => setCurrentSlide(index)}
+                        ></button>)
+                    : null
+                }
+            </span>
         </div>
-    ) 
+    );
 }
