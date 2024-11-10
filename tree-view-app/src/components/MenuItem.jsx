83b80a5d1813c426f0c3df4a1dec009cdf1ba44f
gtlegend <yusuf_ar@live.com>
2024-11-10 15:07:30 -0400

    added react icons to tree menu.

--- tree-view-app/src/components/MenuItem.jsx
+++ tree-view-app/src/components/MenuItem.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import MenuList from "./MenuList"
+import {FaMinus, FaPlus} from 'react-icons/fa'
 
 export default function MenuItem({item}){ 
     /* item data passed from MenuList component after mapping there */
@@ -21,7 +22,7 @@ export default function MenuItem({item}){
                 {
                     item && item.children && item.children.length ? <span onClick={()=>handleToggleChildren(item.label)}>
                     {
-                        displayCurrentChild[item.label] ? '-' : '+'
+                        displayCurrentChild[item.label] ? <FaMinus/> : <FaPlus/>
                     }
                     </span> :null 
                 }
