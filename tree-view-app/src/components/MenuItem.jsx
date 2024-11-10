8736dbb736bba1f586958d880a01b5ccc2b4b113
gtlegend <yusuf_ar@live.com>
2024-11-10 14:59:26 -0400

    Expand or close child coponent on click.

--- tree-view-app/src/components/MenuItem.jsx
+++ tree-view-app/src/components/MenuItem.jsx
@@ -5,12 +5,30 @@ export default function MenuItem({item}){
     /* item data passed from MenuList component after mapping there */
 
     const [displayCurrentChild, setDisplayCurrentChild] = useState({});
+
+    function handleToggleChildren(getCurrentLabel){
+        setDisplayCurrentChild({
+            ...displayCurrentChild,
+            [getCurrentLabel] : !displayCurrentChild[getCurrentLabel]
+        });
+    }
+
+    console.log(displayCurrentChild);
     return (
         <li className="menu-item-container">
-            <p>{item.label}</p>
+            <div>
+                <p>{item.label}</p>
+                {
+                    item && item.children && item.children.length ? <span onClick={()=>handleToggleChildren(item.label)}>
+                    {
+                        displayCurrentChild[item.label] ? '-' : '+'
+                    }
+                    </span> :null 
+                }
+            </div>
 
             {
-                item && item.children && item.children.length > 0 ? ( /* checks if the item in data.js has a children */
+                item && item.children && item.children.length > 0 && displayCurrentChild[item.label] ? ( /* checks if the item in data.js has a children */
                     <MenuList list={item.children} />
                 ) : null
             }
