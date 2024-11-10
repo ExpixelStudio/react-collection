d86a2b9af8746c33f6dd50052169895ed84f8be5
gtlegend <yusuf_ar@live.com>
2024-11-10 00:38:21 -0400

    Added new component items.

---
+++ tree-view-app/src/components/MenuItem.jsx
@@ -0,0 +1,9 @@
+
+
+export default function MenuItem({item}){
+    return (
+        <div className="menu-item-container">
+            Menu Item
+        </div>
+    )
+}
---
+++ tree-view-app/src/components/index.jsx
@@ -0,0 +1,13 @@
+
+
+export default function TreeView({menus=[]}){
+
+
+    return (
+        <div className="tree-view-container">
+            {
+                
+            }
+        </div>
+    )
+}
