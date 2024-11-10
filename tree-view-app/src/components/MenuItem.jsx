f6005f6e71feeddb74030356e0324ff31c33d3a3
gtlegend <yusuf_ar@live.com>
2024-11-10 01:36:39 -0400

    Updated logic to render as a list and show children of displayed info if there are any.

--- tree-view-app/src/components/MenuItem.jsx
+++ tree-view-app/src/components/MenuItem.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
+import MenuList from "./MenuList"
 
+export default function MenuItem({item}){ 
+    /* item data passed from MenuList component after mapping there */
 
-export default function MenuItem({item}){
+    const [displayCurrentChild, setDisplayCurrentChild] = useState({});
     return (
-        <div className="menu-item-container">
-            Menu Item
-        </div>
+        <li className="menu-item-container">
+            <p>{item.label}</p>
+
+            {
+                item && item.children && item.children.length > 0 ? ( /* checks if the item in data.js has a children */
+                    <MenuList list={item.children} />
+                ) : null
+            }
+        </li>
     )
 }
