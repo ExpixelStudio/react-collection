494cd854b938d5cd39b36ef92576b11ef9b65c57
gtlegend <yusuf_ar@live.com>
2024-11-14 07:06:01 -0400

    Map and render item data to DOM. added methods to track scroll height and position.

--- custom-scroll-indicator-app/src/components/index.jsx
+++ custom-scroll-indicator-app/src/components/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 export default function ScrollIndicator({url}){
@@ -6,6 +6,7 @@ export default function ScrollIndicator({url}){
    const [data, setData] = useState([]);
    const [loading, setLoading] = useState(false);
    const [errorMsg, setErrorMsg] = useState('');
+   const [scrollPercentage, setScrollPercentage] = useState(0);
    
    async function fetchData(getUrl){
       try{
@@ -13,7 +14,12 @@ export default function ScrollIndicator({url}){
          const response = await fetch(getUrl);
          const data = await response.json();
          
-         console.log(data);
+        
+
+         if (data && data.products && data.products .length > 0) {
+            setData(data.products);
+            setLoading(false);
+         }
 
       }catch(e) {
          console.log(e);
@@ -23,11 +29,42 @@ export default function ScrollIndicator({url}){
 
    useEffect(() =>{
       fetchData(url);
-   },[url])
+   },[url]);
+
+   function handleScrollPercentage(){
+      console.log(document.body.scrollTop, 
+         document.documentElement.scrollTop, 
+         document.documentElement.scrollHeight, 
+         document.documentElement.clientHeight
+      );
+
+      const howMuchScrolled = document.body.scrolTop || document.documentElement.scrollTop;
+      const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+      setScrollPercentage((howMuchScrolled/height)*100);
+   }  
+
+   useEffect(() => {
+      window.addEventListener('scroll', handleScrollPercentage)
+      return () =>{
+         window.removeEventListener('scroll', () => {})
+      }
+   },[])
+
+   /* console.log(data,loading); */
 
    return (
       <div>
-
+         <h1>Custom Scroll Indicator</h1>
+         <div className="data-container">
+            { 
+               data && data.length > 0 
+               ? data.map((dataItem) => <p>{dataItem.title}</p> )
+               : null 
+            }
+         </div>
       </div>
    );
+
+   
 }
