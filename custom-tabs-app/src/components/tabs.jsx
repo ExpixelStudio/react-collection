b175f1cca015bb00994cf05910cb7799b881dbb5
gtlegend <yusuf_ar@live.com>
2024-11-15 06:29:11 -0400

    logic to map tab data objects and get index of clicked tab.

--- custom-tabs-app/src/components/tabs.jsx
+++ custom-tabs-app/src/components/tabs.jsx
@@ -1,8 +1,35 @@
+import './tabs.css';
+import { useState } from "react";
 
 
+export default function Tabs({tabsContent, onChange}){ 
 
-export default function Tabs(){ 
+   const [currentTabIndex, setCurrentTabIndex] = useState(0);
+
+   function handleOnClick(getCurrentIndex){
+      setCurrentTabIndex(getCurrentIndex);
+      onChange(getCurrentIndex);
+   }
+
    return (
-      <div></div>
+      <div className="wrapper">
+         <div className="heading">
+            {
+               tabsContent.map((tabItem, index) => (
+                  <div className={`tab-item ${currentTabIndex === index ? 'active' : ''}`} onClick={()=> handleOnClick(index)} key={tabItem.label}>
+                     <span className="label">{tabItem.label}</span>
+                  </div>
+            ))}
+
+         </div>
+
+         <div className="content">
+            {
+               tabsContent[currentTabIndex] && tabsContent[currentTabIndex].content
+            }
+
+         </div>
+
+      </div>
    );
 }
