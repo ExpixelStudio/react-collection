cbcb8a1dc74c9576c87da2d9048787573bf5aa31
gtlegend <yusuf_ar@live.com>
2024-11-16 16:52:25 -0400

    added jsx for displaying modal content

--- custom-modal-popup-app/src/components/modal.jsx
+++ custom-modal-popup-app/src/components/modal.jsx
@@ -1,7 +1,28 @@
 
 
-export default function Modal(){
+export default function Modal({id, header, body, footer}){
    return (
-      <div>Modal</div>
+      <div id= {id || 'Modal'} className="modal">Modal
+        
+         <div className="modal-content">
+            <div className="header">
+               <span className="close-modal-icon">&times;</span>
+               <h2>{header ? header : 'Header'}</h2>
+            </div>
+
+            <div className="body">
+               {
+                  body ? body : 
+                     <p>This is our Modal Body</p>
+               }
+            </div>
+
+               <div className="footer">
+                  {
+                     footer ? footer : <h2>Footer</h2>
+                  }
+               </div>
+         </div>
+      </div>
    )
 }
