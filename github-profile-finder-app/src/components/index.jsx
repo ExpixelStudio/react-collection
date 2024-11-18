817df28870261111192a836f0fb54097c8ced5cd
gtlegend <yusuf_ar@live.com>
2024-11-17 22:12:50 -0400

    Fetch user data on search button click.

--- github-profile-finder-app/src/components/index.jsx
+++ github-profile-finder-app/src/components/index.jsx
@@ -1,25 +1,40 @@
 import { useEffect, useState } from "react"
-
+import UserCard from "./UserCard";
 
 
 export default function GithubProfileFinder(){
 
    const [userName, setUsername] = useState('ExpixelStudio');
+   const [userData, setUserData] = useState(null);
+   const [loading, setLoading] = useState(false);
    
-   async function fetchGithubUserData(params) {
+   async function fetchGithubUserData() {
+      setLoading(true);
       const response = await fetch(`https://api.github.com/users/${userName}`);
       const data = await response.json();
+
+      if(data){
+         setUserData(data);
+         setLoading(false);
+         setUsername('');
+      }
 
       console.log(data);
    }
 
    function handleSubmit(){
-
+      fetchGithubUserData();
    }
 
    useEffect(()=>{
       fetchGithubUserData();
    },[])
+
+   if(loading){
+      return (
+         <h1>Loading data ! Please wait.</h1>
+      )
+   }
 
    return (
       <div className="github-profile-container">
@@ -35,6 +50,9 @@ export default function GithubProfileFinder(){
             
             <button onClick={handleSubmit}>Search</button>
          </div>
+         {
+            userData !== null ? <UserCard user={userData} /> : null
+         }
       </div>
-   )
+   );
 }
