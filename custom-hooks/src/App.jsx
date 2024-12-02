6bffd52ce03f348cd30ae6ab3bce437fb06dd4cc
gtlegend <yusuf_ar@live.com>
2024-12-02 16:32:15 -0400

    Render test component to dom.

--- custom-hooks/src/App.jsx
+++ custom-hooks/src/App.jsx
@@ -1,4 +1,4 @@
-
+import UseFetchHookTest from './components/UseFetch/test'
 import './App.css'
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      
+      <UseFetchHookTest />
     </>
   )
 }
