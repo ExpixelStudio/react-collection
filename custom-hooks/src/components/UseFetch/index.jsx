a25ac25ff0536fbef347dd3f7048c97ceba80897
gtlegend <yusuf_ar@live.com>
2024-12-02 16:33:49 -0400

    Logic for capturing and handling data for UseFetch custom hook.

--- custom-hooks/src/components/UseFetch/index.jsx
+++ custom-hooks/src/components/UseFetch/index.jsx
@@ -1,5 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function useFetch(url, options = {}) {
-   
+   //manage 3 states
+   const [data, setData] = useState(null);
+   const [pending, setPending] = useState(false); //Loading / setLoading
+   const [error, setError] = useState(null);
+
+
+   async function fetchData(){
+      setPending(true);
+      try{
+         const response = await fetch(url,{...options});
+         if(!response.ok) throw new Error(response.statusText);
+
+         const result = await response.json();
+         setData(result);
+         setError(null);
+         setPending(false);
+      } catch(e) {
+         setError(`${e}. Some Error Occured!`);
+         setPending(false);
+      }
+   }
+
+   useEffect(()=>{
+      fetchData();
+
+   },[url])
+
+   return {data, error, pending};
 }
