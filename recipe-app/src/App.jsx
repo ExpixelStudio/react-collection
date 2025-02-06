3ec45001d899d190d9f882dbcbaa00e6262eef3e
gtlegend <yusuf_ar@live.com>
2025-02-06 11:45:51 -0400

    Added components and pages folders. Imported react-router-dom and basic page Route setup.

--- recipe-app/src/App.jsx
+++ recipe-app/src/App.jsx
@@ -1,13 +1,24 @@
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/navbar";
+import Home from "./pages/home";
+import Favourites from "./pages/favourites";
+import Details from "./pages/details";
 
 function App() {
-
-
   return (
     <>
-      <div>Something...</div>
+      <div className="min-h-screen p-6 bg-white text-yellow-600 text-lg">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourites />} />
+          <Route path="/details/:id" element={<Details />} /> {/* /:id dynamic page */}
+  
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
