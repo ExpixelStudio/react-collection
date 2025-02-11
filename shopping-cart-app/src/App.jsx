9953af35fdac4b17ea2527f3f08b740643dde797
gtlegend <yusuf_ar@live.com>
2025-02-11 19:55:29 -0400

    Added components and pages folders and associated Components. Setup browser router on App component. Set up navbar in Header component with links.

--- shopping-cart-app/src/App.jsx
+++ shopping-cart-app/src/App.jsx
@@ -1,12 +1,20 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Header from "./components/header";
 /* import './App.css' */
 
 function App() {
  
   return (
     <>
-      <div>App</div>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
---
+++ shopping-cart-app/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+// export default function
+export default function Home() {
+  return (
+     <div>
+         Home
+     </div>
+  );
+};
+
+
+
+
