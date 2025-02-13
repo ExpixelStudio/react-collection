179cee330b88b546638e5d021cc50a273728d9b4
gtlegend <yusuf_ar@live.com>
2025-02-13 12:51:13 -0400

    Home component modifited to ftech api data and display loading while fetching. Also Pass data from Home to ProductTile component where it is formatted and rendered.

--- shopping-cart-app/src/pages/Home.jsx
+++ shopping-cart-app/src/pages/Home.jsx
@@ -1,9 +1,51 @@
+import { useEffect, useState } from "react";
+import ProductTile from "../components/product-tile";
+
+import 'ldrs/bouncy';
 
 // export default function
 export default function Home() {
-  return (
+  
+   const [products, setProducts] = useState([]); 
+   const [loading, setLoading] = useState(false);
+
+   useEffect(()=>{
+      async function fetchListOfProducts(){
+         setLoading(true);
+         const response = await fetch(`https://fakestoreapi.com/products?limit=10`)
+         const data = await response.json();
+
+         if(data) {
+            setLoading(false);
+            setProducts(data);
+         }
+         
+         console.log(data);
+      }
+      fetchListOfProducts()
+   },[]);
+  
+   return (
      <div>
-         Home
+         {
+            loading ? ( 
+               <div className="min-h-screen w-full flex justify-center items-center">
+                  <l-bouncy
+                    size="120"
+                    speed="1.75"
+                    color='rgb(120,79,196)' 
+                  ></l-bouncy>
+            </div> 
+            ): (
+            <div className="min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl mx-auto p-3 ">
+               {
+                  products && products.length ?(
+                     products.map((item)=> <ProductTile product={item}/>)
+                  ) : null   
+               }
+            </div>
+            )            
+         }
      </div>
   );
 };
