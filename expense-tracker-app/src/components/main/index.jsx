5d7573b6970f89b89f444c968fce257ebe150b7b
gtlegend <yusuf_ar@live.com>
2025-04-07 19:12:03 -0400

    Update main component index. Added two new components expense-view and summary

--- expense-tracker-app/src/components/main/index.jsx
+++ expense-tracker-app/src/components/main/index.jsx
@@ -1,9 +1,30 @@
-
+import { Flex, Heading, Button} from "@chakra-ui/react";
+import ExpenseView from "../expense-view";
+import Summary from "../summary";
 
 export default function Main() {
   return (
-     <div>
-         Main
-     </div>
+     <Flex textAlign={'center'} flexDirection={'column'} pr={'5'} pl={'5'} >
+        <Flex alignItems = {'center'} justifyContent={'space-between'} mt={'12'}>
+          <Heading color ={'blue.400'} display={['none', 'block', 'block']}>
+            Expense Tracker
+          </Heading>
+
+        <Flex alignItems={'center'}>
+          <Button 
+          bg={'blue.400'}
+          color={'black'}
+          ml={'4'}
+          >  
+            Add New Transaction
+          </Button>  
+        </Flex>
+        </Flex>
+        <Summary/>
+        <Flex>
+          <ExpenseView/>  
+          
+        </Flex>
+     </Flex>
   );
 };
---
+++ expense-tracker-app/src/components/summary/index.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Summary() {
+  return (
+     <div>
+         <h1>Summary Component</h1>;
+     </div>
+  );
+};
