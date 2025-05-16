19c8b9e25dc945bcd9cd3895e84ee0ba5f6a594c
gtlegend <yusuf_ar@live.com>
2025-05-16 06:34:37 -0400

    updates to  main and summary components.

--- expense-tracker-app/src/components/expense-view/index.jsx
+++ expense-tracker-app/src/components/expense-view/index.jsx
@@ -3,7 +3,7 @@
 export default function ExpenseView() {
   return (
      <div>
-         <h1>Expense View</h1>;
+         <h1>Expense View</h1>
      </div>
   );
 };
--- expense-tracker-app/src/components/main/index.jsx
+++ expense-tracker-app/src/components/main/index.jsx
@@ -1,30 +1,31 @@
-import { Flex, Heading, Button} from "@chakra-ui/react";
+import { Flex, Heading, Button } from "@chakra-ui/react";
 import ExpenseView from "../expense-view";
 import Summary from "../summary";
 
 export default function Main() {
   return (
-     <Flex textAlign={'center'} flexDirection={'column'} pr={'5'} pl={'5'} >
-        <Flex alignItems = {'center'} justifyContent={'space-between'} mt={'12'}>
-          <Heading color ={'blue.400'} display={['none', 'block', 'block']}>
-            Expense Tracker
-          </Heading>
+    <Flex textAlign={"center"} flexDirection={"column"} pr={"5"} pl={"5"}>
+      <Flex alignItems={"center"} justifyContent={"space-between"} mt={"12"}>
+        <Heading color={"blue.400"} display={["none", "block", "block"]}>
+          Expense Tracker
+        </Heading>
 
-        <Flex alignItems={'center'}>
-          <Button 
-          bg={'blue.400'}
-          color={'black'}
-          ml={'4'}
-          >  
+        <Flex alignItems={"center"}>
+          <Button bg={"blue.400"} color={"black"} ml={"4"}>
             Add New Transaction
-          </Button>  
+          </Button>
         </Flex>
-        </Flex>
-        <Summary/>
-        <Flex>
-          <ExpenseView/>  
-          
-        </Flex>
-     </Flex>
+      </Flex>
+      <Summary />
+      <Flex
+        w="full"
+        alignItems={"flex-start"}
+        justifyContent={"space-evenly"}
+        flexDirection={["column", "column", "column", "row", "row"]}
+      >
+        <ExpenseView />
+        <ExpenseView />
+      </Flex>
+    </Flex>
   );
-};
+}
