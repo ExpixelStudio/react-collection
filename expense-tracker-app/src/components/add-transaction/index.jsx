eb89d6ca574fd0e4e18d747d648e58fdc38ebe9d
gtlegend <yusuf_ar@live.com>
2025-05-18 14:47:23 -0400

    Working on add-transaction component

---
+++ expense-tracker-app/src/components/add-transaction/index.jsx
@@ -0,0 +1,13 @@
+import {Modal, ModalContent, ModalHeader, ModalOverlay} from "@chakra-ui/react";
+
+export default function TransactionForm() {
+  return (
+     <Modal>
+        <form>
+          <Modal>
+            
+          </Modal>
+        </form>
+     </Modal>
+  );
+};
