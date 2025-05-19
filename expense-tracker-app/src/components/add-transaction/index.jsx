ccea339b1585cbad02267b54f07a89feb02d0b5a
gtlegend <yusuf_ar@live.com>
2025-05-18 22:57:25 -0400

    Further layout update to add-transaction component

--- expense-tracker-app/src/components/add-transaction/index.jsx
+++ expense-tracker-app/src/components/add-transaction/index.jsx
@@ -1,13 +1,62 @@
-import {Modal, ModalContent, ModalHeader, ModalOverlay} from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalCloseButton,
+  useDisclosure,
+  FormControl,
+  FormLabel,
+  Input,
+  RadioGroup,
+  Radio,
+  Button,
+} from "@chakra-ui/react";
 
 export default function TransactionForm() {
   return (
-     <Modal>
-        <form>
-          <Modal>
-            
-          </Modal>
-        </form>
-     </Modal>
+    <Modal>
+      <form>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add New Transaction</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Enter Description</FormLabel>
+              <Input
+                placeholder="Enter Transaction Description"
+                name="description"
+                type="text"
+              />
+            </FormControl>
+
+            <FormControl>
+              <FormLabel>Enter Amount</FormLabel>
+              <Input
+                placeholder="Enter Transaction Amount"
+                name="amount"
+                type="number"
+              />
+            </FormControl>
+
+            <RadioGroup mt="5">
+              <Radio value="income" colorScheme="blue" name="type">
+                Income
+              </Radio>
+              <Radio value="expense" colorScheme="red" name="type">
+                Expense
+              </Radio>
+            </RadioGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button mr={"4"}>Cancle</Button>
+            <Button>Add</Button>
+          </ModalFooter>
+        </ModalContent>
+      </form>
+    </Modal>
   );
-};
+}
