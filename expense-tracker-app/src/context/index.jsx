8cb0eeb15c27b926b88975c50ac4351db167bd3f
gtlegend <yusuf_ar@live.com>
2025-05-23 07:53:18 -0400

    update global context component to track and managae the state of expenses values.

--- expense-tracker-app/src/context/index.jsx
+++ expense-tracker-app/src/context/index.jsx
@@ -2,10 +2,34 @@ import { createContext, useState } from "react";
 
 export const GlobalContext = createContext(null);
 
-export default function GlobalState({children}) {
-  const [state, setState] = useState(""); // Define some initial state
+export default function GlobalState({ children }) {
+  const [formData, setFormData] = useState({
+    type: "expense",
+    amount: 0,
+    description: "",
+  }); // Define some initial state
 
+  const [value, setValue] = useState("expense");
+  const [totalExpense, setTotalExpense] = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [allTransactions, setAllTransactions] = useState([]);
+  
   return (
-     <GlobalContext.Provider value={{state,setState}}>{children}</GlobalContext.Provider>
+    <GlobalContext.Provider
+      value={{
+        formData,
+        setFormData,
+        totalExpense,
+        setTotalExpense,
+        totalIncome,
+        setTotalIncome,
+        value,
+        setValue,
+        allTransactions,
+        setAllTransactions,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
   );
-};
+}
